872adcf27650143cedafa0ac7a07ff44ff5264d2
landonas@hawaii.edu <landonas@hawaii.edu>
2018-07-19 22:31:08 -1000

    Documenting work

--- multiplication.py
+++ multiplication.py
@@ -1,9 +1,13 @@
 import numpy as np
 
-A = np.arange(1,10).reshape(3,3)
-B = np.arange(1,10).reshape(3,3)
-print(A)
-print(B)
+A = np.arange(1,10).reshape(3,3)    # creating an array 1 - 10 and shaping it into a 3*3 matrix
+B = np.arange(1,10).reshape(3,3)    # creating an array 1 - 10 and shaping it into a 3*3 matrix
 
-m = np.dot(A,B)
-print(m)
+m = np.dot(A,B)                     # multiplying both arrays 
+print(m)                            # printing result
+'''
+RESULT:
+[[ 30  36  42]
+ [ 66  81  96]
+ [102 126 150]]
+'''
