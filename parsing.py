b46d2b97b685480ea8a7ff1b94e67a17f804e161
landonas@hawaii.edu <landonas@hawaii.edu>
2018-07-19 19:58:44 -1000

    more print lines

--- parsing.py
+++ parsing.py
@@ -10,3 +10,4 @@ print(a)                  # Prints "[5, 2, 3]"
 b = np.array([[1,2,3],[4,5,6]])    # Create a rank 2 array
 print(b.shape)                     # Prints "(2, 3)"
 print(b[0, 0], b[0, 1], b[1, 0])   # Prints "1 2 4"
+print(b[0, 0], b[0, 1], b[1, 0])   # Prints "1 2 4"
