50fe3e6c9f28e44f3bb8f209f834e7c04592ceba
landonas@hawaii.edu <landonas@hawaii.edu>
2018-08-01 21:26:14 -1000

    detials

--- parsing.py
+++ parsing.py
@@ -21,10 +21,10 @@ t = np.zeros((5,5))                 #zeros functions returns matrix filled with 
 
 print(np.eye(3))                    #eye function retuns matrix as specified 
 
-print(np.zeros((10,10)))            
+print(np.zeros((10,10)))            #np zeros
 
 
-newArray = np.array([[1,2,3], [4,5,6] , [7,8,9]])   #rank three array
+newArray = np.array([[1,2,3], [4,5,6] , [7,8,9]])   #rank three array 
 print(newArray)
 
 newArray = np.array([[1,2,3], [4,5,6] , [7,8,9], [10,11,12]]) #rank four array
